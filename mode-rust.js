f9c4f3670cd2ea428c314592b46f39444133748a
depombo <depombo@users.noreply.github.com>
2020-09-05 17:33:17 +0000

    deploy: 96e864e8b0146556c876bfc80a056a7dc6736f88

--- mode-rust.js
+++ mode-rust.js
@@ -89,7 +89,7 @@ var RustHighlightRules = function() {
             regex: /[+\-\/*^.~`!@#$%&|:;<?=][+\-\/*^.~`!@#$%&|:;<>?=]*/
         }],
         "#booleans": [{
-            token: "string.other",
+            token: "constant.numeric",
             regex: /\b(?:true|false)\b/
         }],
         "#doubleQuoteStrings": [{
